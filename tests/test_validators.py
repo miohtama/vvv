a1738ac012cae637720c5b0f6095f593a64e10e2
Mikko Ohtamaa <mikko@opensourcehacker.com>
2012-04-12 01:47:16 +0300

    Don't use 3.2 specific unittest API

--- tests/test_validators.py
+++ tests/test_validators.py
@@ -224,7 +224,7 @@ if __name__ == '__main__':
     else: 
         verbosity = 0
     
-    if sys.version_info[0] >= 3 and sys.version_info[1] >= 1:
+    if sys.version_info[0] >= 3 and sys.version_info[1] >= 2:
         unittest.main(verbosity=verbosity)
     else:
         # Older Pythons
