b6da72390bf9b37b4c191098dcb3c2cac1de65c0
Mikko Ohtamaa <mikko@opensourcehacker.com>
2012-04-12 00:33:38 +0300

    See if this change cures problems with Travis CI buildout

--- vvv/utils.py
+++ vvv/utils.py
@@ -14,7 +14,7 @@ import tempfile
 
 # Third party
 import yaml
-from .bzrlib.globster import ExceptionGlobster
+from vvv.bzrlib.globster import ExceptionGlobster
 
 class ShellCommandFailed(Exception):
     """ Executing a shell command failed """
