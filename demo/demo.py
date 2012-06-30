c71d71becb0b6add4545448cd9e3fc75eb98b219
Mikko <mikko@mfabrik.com>
2012-06-30 15:15:21 +0300

    Make sure you can run demo easily

--- demo/demo.py
+++ demo/demo.py
@@ -1,15 +1,1 @@
-class MagicalObject:
-    def doSomething(): pass
-    def doSomethingElse(): pass
-
-def get_magic():
-    """
-    Returns a string, not an object
-    """
-    return MagicalObject()
-
 foobar = get_magic()
-try:
-    foobar.doSomething()
-except:
-    fobar.doSomethingElse()
